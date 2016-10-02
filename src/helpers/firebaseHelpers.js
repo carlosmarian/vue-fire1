18202130bcf1a855e5e94b33b7be6a8bd666d00a
Carlos H. Marian <carlosmarian@gmail.com>
2016-10-02 00:57:40 -0300

    Ajuste

--- src/helpers/firebaseHelpers.js
+++ src/helpers/firebaseHelpers.js
@@ -3,11 +3,11 @@ import firebase from 'firebase'
 export function fireInit(func) {
 	// !!!! Replace with your own Firebase settings !!!!!
 	var config = {        
-				apiKey: "AIzaSyC6tEku5QgXDqP5GjK4ruAMV1MMsTngJRg",
-				authDomain: "vuefiretodo.firebaseapp.com",
-				databaseURL: "https://vuefiretodo.firebaseio.com",
-				storageBucket: "vuefiretodo.appspot.com",
-				messagingSenderId: "156276635439"
+				apiKey: "xxxxxxxxxxxxxxx",
+				authDomain: "xxxxxxxxxxxxxxxxx",
+				databaseURL: "XXXXXXXXXXXXXXXXX",
+				storageBucket: "XXXXXXXXXXXXXXXXXXXX",
+				messagingSenderId: "XXXXXXXXXXXXXXXXXX"
 
 	}
 	firebase.initializeApp(config)
